93487b597531d634f834d010999bd3805f9b9502
Oscar Chetrit <ochetrit@made-f0Ar3s3.clusters.42paris.fr>
2024-07-18 12:44:20 +0200

    builitns norme grrr

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,64 +6,64 @@
 /*   By: ochetrit <ochetrit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 16:29:15 by ochetrit          #+#    #+#             */
-/*   Updated: 2024/07/18 11:50:52 by ochetrit         ###   ########.fr       */
+/*   Updated: 2024/07/18 12:43:16 by ochetrit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-void	print_word_echo(char *str)
+t_token	*print_word_echo(t_token *lst)
 {
-	while (str && *str)
+	int	i;
+
+	i = 0;
+	while (lst && lst->value[i])
 	{
-		if (*str != '\\')
-			write(STDOUT_FILENO, str, 1);
-		if (str)
-			str++;
+		if (lst->value[i] != '\\')
+			write(STDOUT_FILENO, lst->value + i, 1);
+		if (lst->value[i])
+			i++;
 	}
+	if (lst)
+		return (lst->next);
+	return (lst);
 }
 
-t_token	*skip_redirect(t_token *lst)
+t_token	*skip_redirect(t_token *lst, int flag2)
 {
 	if (lst)
 		lst = lst->next;
 	if (lst)
 		lst = lst->next;
+	flag2--;
 	return (lst);
 }
 
 int	ft_echo(t_token *lst, t_env **env)
 {
-	int		flag;
-	int	flag2;
+	int		flag[2];
 	
 	if (lst && lst->type == CMD)
 		lst = lst->next;
 	if (!lst)
 		return (write(STDOUT_FILENO, "\n", 1));
-	flag = is_flag(lst->value);
-	flag2 = 0;
+	flag[1] = is_flag(lst->value);
+	flag[0] = 0;
 	while (lst && skip_flag(lst->value))
 		lst = lst->next;
 	while (lst && lst->type != PIPE)
 	{
 		if (lst->type > PIPE)
-    {
-			lst = skip_redirect(lst);
-			flag2--;
-		}
+			lst = skip_redirect(lst, flag[0]);
 		else
 		{
-			print_word_echo(lst->value);
-			lst = lst->next;
-			if (lst && lst->type != PIPE)
-				write(STDOUT_FILENO, " ", 1);
-			flag2 = 1000;
+			lst = print_word_echo(lst);
+			flag[0] = 1000;
 		}
-		if (lst && lst->type == ARG && flag2 > -1)
-			write(1, " ", 1);
+		if (lst && lst->type == ARG && flag[0] > -1)
+			write(STDOUT_FILENO, " ", 1);
 	}
-	if (!flag)
+	if (!flag[1])
 		ft_printf("\n", STDOUT_FILENO);
 	return (exit_status(0, *env), TRUE);
 }
